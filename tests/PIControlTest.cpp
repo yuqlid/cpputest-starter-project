dada7c5c372415870a51663f22ef34a2d527485d
yuqlid <yuqlid@gmail.com>
2024-09-22 23:00:40 +0900

    ゲイン調整とかあるけどPI制御のテストを作ってみる

--- tests/PIControlTest.cpp
+++ tests/PIControlTest.cpp
@@ -32,20 +32,20 @@ TEST_GROUP(PI){void setup(){}
                void teardown(){}};
 
 TEST(PI, test1) {
-  constexpr float fs_hz = 1000.0f;
-  constexpr float fc_hz = 5.0f;
+  constexpr float fs_hz = 5000.0f;
+  constexpr float fc_hz = 20.0f;
 
   FirstOrderLpf<float> lpf(fs_hz, fc_hz);
   PIControllerConfig_t<float> PIConfig;
 
   PIConfig.gain.kp = 2.0f;
-  PIConfig.gain.ki = 0.25f;
+  PIConfig.gain.ki = fs_hz *0.2f;
   PIConfig.bandwitdh = 10.0f;  // テストに影響なし
 
   constexpr float high = 2.0f;
   constexpr float low = -high;
 
-  PIController<float> CurrentController;
+  PIController<float> CurrentController(fs_hz);
 
   if (CurrentController.isGainValid(PIConfig.gain)) {
     CurrentController.setConfig(&PIConfig);
@@ -65,22 +65,23 @@ TEST(PI, test1) {
   for (int i = 0; i < period; ++i) {
     x.at(i) = 0.0f;
     output.at(i) = 0.0f;
-    if (i > 1) {
+    if (i > 0) {
       x.at(i) = 1.0f;
     }
   }
 
-  outfile << "ref,input,input_clamp,output" << std::endl;
+  outfile << "time,ref,input,p,input_clamp,output" << std::endl;
 
   for (size_t i = 0; i < period - 1; ++i) {
     float error = x.at(i) - output.at(i);
     float input = CurrentController.process(error);
+    float p = PIConfig.gain.kp * error;
     float input_clamp = std::clamp(input, low, high);
     float out = lpf.update(input_clamp);
 
     output.at(i + 1) = out;
-    outfile << static_cast<float>(i) / fs_hz << ", " << input << ", "
-            << input_clamp << ", " << out << std::endl;
+    outfile << static_cast<float>(i) / fs_hz << ", " << x.at(i) << ", " << input
+            << ", " << p << ", " << input_clamp << ", " << out << std::endl;
   }
 
   // ファイルを閉じる
