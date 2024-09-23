efb3cb5a86030a0ab5a3a1e2ed79522048615ff3
yuqlid <yuqlid@gmail.com>
2024-09-23 12:55:29 +0900

    PI制御器のテスト修正

--- tests/PIControlTest.cpp
+++ tests/PIControlTest.cpp
@@ -33,22 +33,30 @@ TEST_GROUP(PI){void setup(){}
 
 TEST(PI, test1) {
   constexpr float fs_hz = 5000.0f;
-  constexpr float fc_hz = 20.0f;
+  constexpr float fc_hz = 10.0f;
 
-  FirstOrderLpf<float> lpf(fs_hz, fc_hz);
+  FirstOrderLpf<float> lpf_1(fs_hz, fc_hz);
+  FirstOrderLpf<float> lpf_2(fs_hz, fc_hz);
   PIControllerConfig_t<float> PIConfig;
-
-  PIConfig.gain.kp = 2.0f;
-  PIConfig.gain.ki = fs_hz *0.2f;
-  PIConfig.bandwitdh = 10.0f;  // テストに影響なし
 
   constexpr float high = 2.0f;
   constexpr float low = -high;
+  PIConfig.gain.kp = 2.0f;
+  PIConfig.gain.ki = fs_hz * 0.2f;
+  PIConfig.bandwitdh = 10.0f;  // テストに影響なし
+  PIConfig.out_max = high;
+  PIConfig.out_min = low;
 
-  PIController<float> CurrentController(fs_hz);
+  windup::PIController<float> cc_1(fs_hz);
+  Integral_Anti_windup::PIController<float> cc_2(fs_hz);
 
-  if (CurrentController.isGainValid(PIConfig.gain)) {
-    CurrentController.setConfig(&PIConfig);
+  if (cc_1.isGainValid(PIConfig.gain)) {
+    cc_1.setConfig(&PIConfig);
+  } else {
+    FAIL("Invalid Param");
+  }
+  if (cc_2.isGainValid(PIConfig.gain)) {
+    cc_2.setConfig(&PIConfig);
   } else {
     FAIL("Invalid Param");
   }
@@ -58,30 +66,52 @@ TEST(PI, test1) {
   if (!outfile) {
     FAIL("file open failed");
   }
-  constexpr uint16_t period = 256;
-  std::array<float, period> x;
-  std::array<float, period> output;
+
+  constexpr uint16_t period = 512;
+  std::array<float, period> ref;
+  std::array<float, period> output_1;
+  std::array<float, period> output_2;
+
+  std::fill(output_1.begin(), output_1.end(), 0.0f);
+  std::fill(output_2.begin(), output_2.end(), 0.0f);
 
   for (int i = 0; i < period; ++i) {
-    x.at(i) = 0.0f;
-    output.at(i) = 0.0f;
+    ref.at(i) = 0.0f;
     if (i > 0) {
-      x.at(i) = 1.0f;
+      ref.at(i) = 1.0f;
+    }
+    if (i > 250) {
+      ref.at(i) = -1.0f;
+    }
+    if (i > 400) {
+      ref.at(i) = 0.0f;
     }
   }
 
-  outfile << "time,ref,input,p,input_clamp,output" << std::endl;
+  outfile << "time,ref,input_1,out_1,input_2,out_2" << std::endl;
 
   for (size_t i = 0; i < period - 1; ++i) {
-    float error = x.at(i) - output.at(i);
-    float input = CurrentController.process(error);
-    float p = PIConfig.gain.kp * error;
-    float input_clamp = std::clamp(input, low, high);
-    float out = lpf.update(input_clamp);
+    float error_1 = ref.at(i) - output_1.at(i);
+    float error_2 = ref.at(i) - output_2.at(i);
+    float input_1 = cc_1.process(error_1);
+    float input_2 = cc_2.process(error_2);
+    // float p = PIConfig.gain.kp * error;
+    float input_clamp_1 = std::clamp(input_1, low, high);
+    float input_clamp_2 = std::clamp(input_2, low, high);
+    float out_1 = lpf_1.update(input_clamp_1);
+    float out_2 = lpf_2.update(input_clamp_2);
 
-    output.at(i + 1) = out;
-    outfile << static_cast<float>(i) / fs_hz << ", " << x.at(i) << ", " << input
-            << ", " << p << ", " << input_clamp << ", " << out << std::endl;
+    output_1.at(i + 1) = out_1;
+    output_2.at(i + 1) = out_2;
+    /*
+    outfile << static_cast<float>(i) / fs_hz << ", " << ref.at(i) << ", "
+            << cc_1.getcaclP() << ", " << cc_1.getcalcI() << ", " << out_1
+            << ", " << cc_2.getcaclP() << ", " << cc_2.getcalcI() << ", "
+            << out_2 << ", " << std::endl;
+    */
+    outfile << static_cast<float>(i) / fs_hz << ", " << ref.at(i) << ", "
+            << input_clamp_1 << ", " << out_1 << ", " << input_clamp_2 << ", "
+            << out_2 << ", " << std::endl;
   }
 
   // ファイルを閉じる
