5a1253c2453ee9d82d6f37dc60a97a2b092742a3
Yuki KUSAKABE <yuqlid@gmail.com>
2026-07-01 08:06:53 +0900

    DS402のステートマシンからリセットするテスト追加

--- tests/Ds402DriveTest.cpp
+++ tests/Ds402DriveTest.cpp
@@ -1,5 +1,6 @@
 #include "CppUTest/TestHarness.h"
 
+#include "cia402_manager.hpp"
 #include "ds402_drive.hpp"
 #include "fault_log.hpp"
 #include "fault_manager.hpp"
@@ -7,10 +8,23 @@
 namespace {
 
 bool power_supply_ready = false;
+constexpr uint16_t kFaultResetControlWord = 0b10000000;
 
 void clearFaultState() {
   getFaultLog().notifyRecovered();
   getFaultLog().clearHistory();
+}
+
+void syncActiveFaultToDs402(ds402::StateMachine& machine) {
+  if (shouldRequestDs402Fault(machine.getState(), hasActiveFault())) {
+    machine.requestFault();
+  }
+}
+
+void moveActiveFaultToDs402Fault(ds402::StateMachine& machine) {
+  syncActiveFaultToDs402(machine);
+  CHECK_TRUE(machine.getState() == ds402::State::FaultReactionActive());
+  CHECK_TRUE(machine.update() == ds402::State::Fault());
 }
 
 }  // namespace
@@ -59,4 +73,51 @@ TEST(AppDS402Drive, InitializeHoldsUnsupportedFault) {
   LONGS_EQUAL(static_cast<long>(ErrorCode::Code::kOverCurrent),
               static_cast<long>(
                   static_cast<ErrorCode::Code>(getActiveFaultCode())));
+}
+
+TEST(AppDS402Drive,
+     FaultResetReturnsToSwitchOnDisabledWhenVoltageFaultRecovered) {
+  AppDS402Drive drive;
+  ds402::StateMachine machine{drive};
+  power_supply_ready = true;
+  CHECK_TRUE(machine.update() == ds402::State::SwitchOnDisabled());
+
+  triggerFault(ErrorCode::DCLinkUnderVoltage());
+  moveActiveFaultToDs402Fault(machine);
+
+  machine.setControlWord(kFaultResetControlWord);
+
+  CHECK_TRUE(machine.update() == ds402::State::SwitchOnDisabled());
+  CHECK_FALSE(hasActiveFault());
+}
+
+TEST(AppDS402Drive, FaultResetHoldsFaultWhenVoltageFaultIsNotRecovered) {
+  AppDS402Drive drive;
+  ds402::StateMachine machine{drive};
+  power_supply_ready = true;
+  CHECK_TRUE(machine.update() == ds402::State::SwitchOnDisabled());
+
+  triggerFault(ErrorCode::DCLinkUnderVoltage());
+  power_supply_ready = false;
+  moveActiveFaultToDs402Fault(machine);
+
+  machine.setControlWord(kFaultResetControlWord);
+
+  CHECK_TRUE(machine.update() == ds402::State::Fault());
+  CHECK_TRUE(hasActiveFault());
+}
+
+TEST(AppDS402Drive, FaultResetHoldsFaultForUnsupportedFault) {
+  AppDS402Drive drive;
+  ds402::StateMachine machine{drive};
+  power_supply_ready = true;
+  CHECK_TRUE(machine.update() == ds402::State::SwitchOnDisabled());
+
+  triggerFault(ErrorCode::OverCurrent());
+  moveActiveFaultToDs402Fault(machine);
+
+  machine.setControlWord(kFaultResetControlWord);
+
+  CHECK_TRUE(machine.update() == ds402::State::Fault());
+  CHECK_TRUE(hasActiveFault());
 }
